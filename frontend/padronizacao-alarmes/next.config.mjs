ad930a4e026ee1b7250b246f0bc41a31e7485015
Magno Gouvêa <94480802+magnogouvea@users.noreply.github.com>
2024-10-17 16:04:46 -0300

    Update next.config.mjs

--- frontend/padronizacao-alarmes/next.config.mjs
+++ frontend/padronizacao-alarmes/next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
       },
     ];
   },
+  env: {
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_NAME: process.env.DB_NAME,
+  },
 };
 
 export default nextConfig;
